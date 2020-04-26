f9346af7ee615d0d887c60e8d8420860c9900b8e
Samuel Samir <s.samir@intcore.net>
2020-04-26 18:58:09 +0200

    add ios code

--- ios/Runner/AppDelegate.h
+++ ios/Runner/AppDelegate.h
@@ -1,6 +1,17 @@
 #import <Flutter/Flutter.h>
 #import <UIKit/UIKit.h>
 
-@interface AppDelegate : FlutterAppDelegate
+#define kSDKKey      @"bb2bD7qXUptIQkhprDt53ZfmjdBXMFEQtQnp"
+#define kSDKSecret   @"rWitvIADSG2rmBXWkpIDW0iF98u6fILaDeUf"
+#define kSDKDomain   @"zoom.us"
 
+//@interface AppDelegate : FlutterAppDelegate
+//
+//@end
+
+@interface AppDelegate :  UIResponder <UIApplicationDelegate, UIAlertViewDelegate, MobileRTCPremeetingDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+- (UIViewController *)topViewController;
 @end
